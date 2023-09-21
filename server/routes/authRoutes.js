2ca0ab19454244a574c5616fae63b14153cb19fe
Dilson <juniorsordi@gmail.com>
2023-09-21 16:50:23 -0300

    alterando biblioteca de parse do OFX

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const jwt = require("jsonwebtoken");
 var fs = require("fs");
-const ofx = require('ofx-convertjs');
+var xml2js = require('xml2js');
+//const ofx = require('ofx-convertjs');
+var ofxParse = require('../infra/ofx2');
 var multer = require('multer');
 const upload = multer({ dest: "uploads/" });
 
@@ -83,7 +85,88 @@ router.post("/ofx/upload", upload.array("file"), uploadFiles);
 
 function uploadFiles(req, res) {
     let filename = req.files;
-    const file = fs.readFileSync(filename[0].path, 'utf8')
+    if(filename.length == 0) {
+        return;
+    }
+    const file = fs.readFileSync(filename[0].path, 'utf8');
+    ofxParse.parse(file).then(ofxData => {
+        let infoSaldo = {
+            valor: ofxData.OFX.BANKMSGSRSV1.STMTTRNRS.STMTRS.LEDGERBAL.BALAMT,
+            data: utils.formatDateDMY(ofxData.OFX.BANKMSGSRSV1.STMTTRNRS.STMTRS.LEDGERBAL.DTASOF)
+        };
+
+        let resumo = {
+            creditos: 0,
+            debitos: 0
+        };
+
+        let listaTransacoes = ofxData.OFX.BANKMSGSRSV1.STMTTRNRS.STMTRS.BANKTRANLIST.STMTTRN;
+        let transacoes2 = [];
+        let saldoInicial = parseFloat(ofxData.OFX.BANKMSGSRSV1.STMTTRNRS.STMTRS.LEDGERBAL.BALAMT);
+        let i = 0;
+        listaTransacoes.map(function (item) {
+            if (i == 0) {
+                item.saldo = parseFloat(item.TRNAMT);
+            } else {
+                item.saldo = listaTransacoes[i - 1].saldo + parseFloat(item.TRNAMT);
+            }
+            item.valor = parseFloat(item.TRNAMT);
+            if(item.valor > 0) {
+                resumo.creditos += item.valor;
+            } else {
+                resumo.debitos += item.valor;
+            }
+            item.dataoperacao = item.DTPOSTED.substr(6, 2) + '/' + item.DTPOSTED.substr(4, 2) + '/' + item.DTPOSTED.substr(0, 4);
+            
+            transacoes2.push({
+                dataoperacao: item.dataoperacao,
+                descricao: item.MEMO,
+                nome: item.NAME,
+                valor: parseFloat(item.TRNAMT),
+                tipo: (item.TRNTYPE == 'OTHER' ? (item.valor > 0 ? 'CREDITO' : 'DEBITO') : item.TRNTYPE + 'O')
+            });
+            saldoInicial += parseFloat(item.TRNAMT);
+            i++;
+        });
+
+        res.json({ message: "Successfully uploaded files", saldo: infoSaldo, transacoes: transacoes2, resumo });
+    });
+    
+    /*
+    ofxParse.parse(file).then(ofxData => {
+        console.log(ofxData.OFX.BANKMSGSRSV1.LEDGERBAL);
+        let infoSaldo = {
+            valor: ofxData.OFX.BANKMSGSRSV1.LEDGERBAL.BALAMT,
+            data: utils.formatDateDMY(ofxData.OFX.BANKMSGSRSV1.LEDGERBAL.DTASOF)
+        };
+        let listaTransacoes = ofxData.OFX.BANKMSGSRSV1.BANKTRANLIST.STMTTRN;
+        let i = 0;
+        let saldoInicial = parseFloat(ofxData.OFX.BANKMSGSRSV1.LEDGERBAL.BALAMT);
+        let transacoes2 = [];
+        listaTransacoes.map(function (item) {
+            if (i == 0) {
+                item.saldo = parseFloat(item.TRNAMT);
+            } else {
+                item.saldo = listaTransacoes[i - 1].saldo + parseFloat(item.TRNAMT);
+            }
+            item.valor = parseFloat(item.TRNAMT);
+            item.dataoperacao = item.DTPOSTED.substr(6, 2) + '/' + item.DTPOSTED.substr(4, 2) + '/' + item.DTPOSTED.substr(0, 4);
+
+            transacoes2.push({
+                dataoperacao: item.dataoperacao,
+                descricao: item.MEMO,
+                nome: item.NAME,
+                valor: parseFloat(item.TRNAMT),
+                tipo: (item.TRNTYPE == 'OTHER' ? (item.valor > 0 ? 'CREDITO' : 'DEBITO') : item.TRNTYPE + 'O')
+            });
+            saldoInicial += parseFloat(item.TRNAMT);
+            i++;
+        });
+
+        res.json({ message: "Successfully uploaded files", saldo: infoSaldo, transacoes: transacoes2, saldoFinal: saldoInicial });
+    });
+    //*/
+    /*
     const data = ofx.toJson(file);
 
     const infoSaldo = {
@@ -113,12 +196,13 @@ function uploadFiles(req, res) {
         saldoInicial += parseFloat(item.TRNAMT);
         i++;
     });
-    res.json({ message: "Successfully uploaded files", saldo: infoSaldo, transacoes: transacoes2, saldoFinal: saldoInicial });
+    //*/
+    //res.json({ message: "Successfully uploaded files", saldo: infoSaldo, transacoes: transacoes2, saldoFinal: saldoInicial });
 }
 
 router.get("/ofx/upload", async function (req, res, next) {
-    //*
-    const file = fs.readFileSync('extrato_202307.ofx', 'utf8')
+    /*
+    
     const data = ofx.toJson(file);
 
     const infoSaldo = {
@@ -136,7 +220,19 @@ router.get("/ofx/upload", async function (req, res, next) {
         item.dataoperacao = item.DTPOSTED.substr(6, 2) + '/' + item.DTPOSTED.substr(4, 2) + '/' + item.DTPOSTED.substr(0, 4);
         i++;
     });
-    res.status(200).json( { saldo: infoSaldo, transacoes: listaTransacoes});
+    //*/
+    //const ofxString = readFile("bank-statement.ofx");
+    const file = fs.readFileSync('extrato_202307.ofx', 'utf8')
+    ofxParse.parse(file).then(ofxData => {
+        let infoSaldo = {};
+        const statementResponse = ofxData.OFX.BANKMSGSRSV1.STMTTRNRS.STMTRS;
+        const accountId = statementResponse.BANKACCTFROM.ACCTID;
+        //const currencyCode = currencyCode = statementResponse.CURDEF;
+        const transactionStatement = statementResponse.BANKTRANLIST.STMTTRN;
+        // do something...
+        res.status(200).json({ saldo: infoSaldo, transacoes: transactionStatement });
+    });
+    
 });
 
 module.exports = router;
