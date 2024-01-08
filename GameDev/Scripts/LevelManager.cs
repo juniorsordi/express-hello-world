1249a9388d2527143918ebc83eb376aac72115f8
DILSON SORDI JUNIOR <10005@tcesc.tc.br>
2024-01-08 17:57:46 -0300

    08/01 TC

--- GameDev/Scripts/LevelManager.cs
+++ GameDev/Scripts/LevelManager.cs
@@ -4,6 +4,12 @@ using System.Collections;
 namespace DSJGames {
 
     public class LevelManager : MonoBehaviour {
+
+        public string levelName;
+        public int levelIndex;
+        Transform playerStartPoint;
+
+        public Event[] events;
         
     }
 
--- GameDev/Scripts/PlayerManager.cs
+++ GameDev/Scripts/PlayerManager.cs
@@ -12,12 +12,22 @@ namespace DSJGames {
         public int nextLevelExperience = 0;
         public bool isDead = false;
 
+        public float healthRegenRate = 0.1f;
+
         public int playerDeaths = 0;
 
         void Start()
         {
             playerMaxHealth = playerHealth;
             calculateNextLevelXP();
+            CheckPlayerStatsByLevel();
+        }
+
+        void Update()
+        {
+            if(healthRegenRate > 0) {
+                healthRegeneration();
+            }
         }
 
         public void TakeDamage(float damage) {
@@ -44,6 +54,21 @@ namespace DSJGames {
         private void calculateNextLevelXP() {
             int temp = playerLevel * 300;
             nextLevelExperience = temp;
+            CheckPlayerStatsByLevel();
+        }
+
+        private void healthRegeneration() {
+            if(playerHealth >= playerMaxHealth) {
+                playerHealth = playerMaxHealth;
+            } else {
+                float tempNewHealth = playerHealth + (1 * healthRegenRate);
+                playerHealth = Mathf.Clamp(tempNewHealth, 0, playerMaxHealth);
+            }
+        }
+
+        private void CheckPlayerStatsByLevel() {
+            float tempHealth = playerMaxHealth + (playerLevel * 50);
+            playerMaxHealth = tempHealth;
         }
 
         private void Die() {
