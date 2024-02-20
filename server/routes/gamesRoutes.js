ee66271f2f3e6c146ef1a12fb7b28555934ec70f
DILSON SORDI JUNIOR <10005@tcesc.tc.br>
2024-02-20 09:37:14 -0300

    adicionado rota nova no modulo games 2

--- server/routes/gamesRoutes.js
+++ server/routes/gamesRoutes.js
@@ -7,7 +7,7 @@ router.get("/", async function (req, res, next) {
     try {
         let hoje = moment().format("YYYY-MM-DD");
         let oldDate = moment().add(-15, "days").format("YYYY-MM-DD");
-        fetch(`https://api.rawg.io/api/games?key=353f4de2e4f54a26812a5ee816408273&dates=${oldDate},${hoje}&page_size=15`)
+        fetch(`https://api.rawg.io/api/games?key=353f4de2e4f54a26812a5ee816408273&dates=${oldDate},${hoje}&page_size=30`)
             .then(response => response.json())
             .then(response => {
                 res.json(response.results);
@@ -24,6 +24,22 @@ router.get("/", async function (req, res, next) {
 router.get("/gamesByPlatform", async function (req, res, next) {
     try {
         fetch("https://api.rawg.io/api/games?key=353f4de2e4f54a26812a5ee816408273&ordering=-released&page_size=50&platforms="+req.query.id)
+            .then(response => response.json())
+            .then(response => {
+                res.json(response.results);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    } catch (err) {
+        console.error(`Error while getting response`, err.message);
+        next(err);
+    }
+});
+
+router.get("/gameInfo", async function (req, res, next) {
+    try {
+        fetch("https://api.rawg.io/api/games/"+req.query.id+"?key=353f4de2e4f54a26812a5ee816408273&page_size=50")
             .then(response => response.json())
             .then(response => {
                 res.json(response.results);
