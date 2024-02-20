83b7255bc79a41d0dc435a6b605659be4cc7235b
DILSON SORDI JUNIOR <10005@tcesc.tc.br>
2024-02-20 09:25:05 -0300

    adicionado rota nova no modulo games

--- server/routes/gamesRoutes.js
+++ server/routes/gamesRoutes.js
@@ -37,9 +37,26 @@ router.get("/gamesByPlatform", async function (req, res, next) {
     }
 });
 
+router.get("/searchGames", async function (req, res, next) {
+    try {
+        let query = encodeURI(req.query);
+        fetch("https://api.rawg.io/api/games?key=353f4de2e4f54a26812a5ee816408273&"+query)
+            .then(response => response.json())
+            .then(response => {
+                res.json(response.results);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    } catch (err) {
+        console.error(`Error while getting response`, err.message);
+        next(err);
+    }
+});
+
 router.get("/platforms", async function (req, res, next) {
     try {
-        fetch("https://api.rawg.io/api/platforms?key=353f4de2e4f54a26812a5ee816408273&page_size=10")
+        fetch("https://api.rawg.io/api/platforms?key=353f4de2e4f54a26812a5ee816408273&page_size=30")
             .then(response => response.json())
             .then(response => {
                 res.json(response.results);
